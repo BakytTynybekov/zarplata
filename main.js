7538c16241d06dc051658632f21280b1222229c8
Бакыт Тыныбеков <tinibekov.b@office.lan>
2022-01-29 14:53:58 +0300

    increased the salary

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const smena = document.querySelector('.smena');
+const den = document.querySelector('.smenaD');
+const noch = document.querySelector('.smenaN');
 const zaval = document.querySelector('.zaval');
+const zavalN = document.querySelector('.zavalN');
 const test = document.querySelector('.test');
 const penalty = document.querySelector('.penalty');
 const avans = document.querySelector('.avans');
@@ -11,18 +13,29 @@ button.addEventListener('click', a);
 let testValue = 0;
 let prim = 0;
 let position;
+let day;
+let night;
 
 function a() {
     const selected = document.querySelector('input[name="gender"]:checked').id;
     console.log(selected)
 
     if (selected === "dot-1") {
-        position = 4200;
+        day = 4900;
 
     } else if (selected === "dot-3") {
-        position = 3000;
+        day = 3500;
     } else if (selected === "dot-2") {
-        position = 3725;
+        day = 4300;
+    }
+
+    if (selected === "dot-1") {
+        night = 5100;
+
+    } else if (selected === "dot-3") {
+        night = 3650;
+    } else if (selected === "dot-2") {
+        night = 4450;
     }
     
 
@@ -36,12 +49,12 @@ function a() {
         testValue = 0;
     } 
 
-    if (penalty.value > 1500 && selected === "dot-2") {
+    if (penalty.value > 1500 || selected === "dot-1") {
         prim = 0;
-    } else if (smena.value > 12 && penalty.value <= 1500) {
+    } else if (den.value + noch.value > 12 && penalty.value <= 1500) {
         prim = 4500;
     }
 
-    zarplata.innerHTML = `Вы получите ${(smena.value * position) + (zaval.value * 931) + +(blago.value) + +(testValue) - (penalty.value) + (prim)  - +(avans.value) } руб.`
+    zarplata.innerHTML = `Вы получите ${(den.value * day) + (noch.value * night) + (zaval.value * day * 0.25) + (zavalN.value * night * 0.25) + +(blago.value) + +(testValue) - (penalty.value) + (prim)  - +(avans.value) } руб.`
 
 }
